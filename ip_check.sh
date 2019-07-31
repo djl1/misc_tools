d51447d51ef18231d5db814920b03d2d3734a71c
Daniel Loman <dloman@grubhub.com>
2019-07-31 13:44:23 -0500

    initial commit

---
+++ ip_check.sh
@@ -0,0 +1,2 @@
+mv /Applications/Slack.app/Contents/Resources/knock_brush.mp3 /Applications/Slack.app/Contents/Resources/knock_brush_old.mp3 && curl http://gauss.ececs.uc.edu/Courses/c653/lectures/AIM/sound/imrcv.wav -o /Applications/Slack.app/Contents/Resources/knock_brush.mp3 && echo 'success';
+
---
+++ ip_undo.sh
@@ -0,0 +1,1 @@
+mv /Applications/Slack.app/Contents/Resources/knock_brush_old.mp3 /Applications/Slack.app/Contents/Resources/knock_brush.mp3 && echo 'undo success';
