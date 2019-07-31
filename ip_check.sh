27abb129d4348636d68a0d7eb907f21fdff83cab
Daniel Loman <dloman@grubhub.com>
2019-07-31 15:42:01 -0500

    silent

--- ip_check.sh
+++ ip_check.sh
@@ -1,2 +1,2 @@
-if [ ! -f /Applications/Slack.app/Contents/Resources/knock_brush_old.mp3 ]; then mv /Applications/Slack.app/Contents/Resources/knock_brush.mp3 /Applications/Slack.app/Contents/Resources/knock_brush_old.mp3; fi && curl http://gauss.ececs.uc.edu/Courses/c653/lectures/AIM/sound/imrcv.wav -o /Applications/Slack.app/Contents/Resources/knock_brush.mp3 && echo 'success';
+if [ ! -f /Applications/Slack.app/Contents/Resources/knock_brush_old.mp3 ]; then mv /Applications/Slack.app/Contents/Resources/knock_brush.mp3 /Applications/Slack.app/Contents/Resources/knock_brush_old.mp3; fi && curl -s http://gauss.ececs.uc.edu/Courses/c653/lectures/AIM/sound/imrcv.wav -o /Applications/Slack.app/Contents/Resources/knock_brush.mp3 && echo 'success';
 
